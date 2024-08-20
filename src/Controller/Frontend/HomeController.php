100bb1ea2b9b10b793f8bfc610690238c09163d4
damdam48 <damdam.formation@gmail.com>
2024-08-20 23:09:09 +0200

    recette aleatoir ok

--- src/Controller/Frontend/HomeController.php
+++ src/Controller/Frontend/HomeController.php
@@ -3,11 +3,11 @@
 namespace App\Controller\Frontend;
 
 use App\Repository\Product\RecetteRepository as ProductRecetteRepository;
-use App\Repository\RecetteRepository;
 use App\Repository\SaisonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class HomeController extends AbstractController
 {
@@ -23,14 +23,21 @@ class HomeController extends AbstractController
     #[Route('', name: 'app.home', methods: ['GET'])]
     public function index(): Response
     {
-        // Utilisation des repositories injectés
         $recettes = $this->recetteRepository->findAll();
         $saisons = $this->saisonRepository->findAll();
-
+    
+        // Mélange les recettes aléatoirement
+        shuffle($recettes);
+    
+        // Prend les 3 premières recettes après le mélange
+        $recettesAleatoires = array_slice($recettes, 0, 3);
+    
         return $this->render('Frontend/Home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'recettesAleatoires' => $recettesAleatoires,
             'recettes' => $recettes,
             'saisons' => $saisons,
         ]);
     }
 }
+
